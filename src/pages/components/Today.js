ef456997cd540065546f78ff406f1639153b3421
sanketvagal <sanketdvagal@gmail.com>
2024-06-20 23:13:12 -0300

    Added API calls for user goals, moved endpoint definition to parent component

--- src/pages/components/Today.js
+++ src/pages/components/Today.js
@@ -1,14 +1,61 @@
 import React, { useEffect, useState } from "react";
-import { fetchActivityData } from "./api";
+import { fetchData } from "./api";
 
 export default function Today({ token }) {
   const [activityData, setActivityData] = useState(null);
+  const BASE_URL = "https://api.fitbit.com/1/user/-/activities";
+  const endpoints = {
+    dailyGoals: `${BASE_URL}/goals/daily.json`,
+    todaySteps: `${BASE_URL}/steps/date/today/1d.json`,
+    // todayCalories: `${BASE_URL}/calories/date/today/1d.json`,
+    // todayDistance: `${BASE_URL}/distance/date/today/1d.json`,
+    todayFloors: `${BASE_URL}/floors/date/today/1d.json`,
+    //   todayMinutesSedentary: `${BASE_URL}/minutesSedentary/date/today/1d.json`,
+    //   todayMinutesVeryActive: `${BASE_URL}/minutesVeryActive/date/today/1d.json`,
+  };
 
   useEffect(() => {
     if (token) {
-      fetchActivityData(token).then((data) => setActivityData(data));
+      fetchData(token, endpoints).then((data) => setActivityData(data));
     }
   }, [token]);
+
+  useEffect(() => {
+    console.log("Component has re-rendered");
+  });
+
+  // if (activityData) {
+  //   console.log("activityData", activityData);
+  // }
+  function extractGoalsAndValues(activityData) {
+    let goals = {};
+    let activityValues = {};
+
+    if (activityData) {
+      activityData.forEach((item) => {
+        if (item.goals) {
+          // check for goals
+          goals = { ...goals, ...item.goals };
+        } else {
+          // check for today values
+          for (let key in item) {
+            if (item[key] instanceof Array) {
+              item[key].forEach((activity) => {
+                activityValues[key.replace("activities-", "")] = activity.value;
+              });
+            }
+          }
+        }
+      });
+    }
+
+    return { goals, activityValues };
+  }
+
+  const { goals, activityValues } = extractGoalsAndValues(activityData);
+
+  console.log("goals", goals);
+  console.log("values", activityValues);
 
   return (
     <div>
@@ -17,6 +64,13 @@ export default function Today({ token }) {
         <div>
           <h4>Activity Data:</h4>
           <pre>{JSON.stringify(activityData, null, 2)}</pre>
+          {Object.keys(activityValues).map((key) => (
+            <div key={key}>
+              <h4>{key}</h4>
+              <p>Goal: {goals[key]}</p>
+              <p>Activity Value: {activityValues[key]}</p>
+            </div>
+          ))}
         </div>
       ) : (
         <p>Loading activity data...</p>
--- src/pages/components/api.js
+++ src/pages/components/api.js
@@ -1,18 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.fitbit.com/1/user/-/activities";
-
-const endpoints = {
-  todaySteps: `${BASE_URL}/steps/date/today/1d.json`,
-  todayCalories: `${BASE_URL}/calories/date/today/1d.json`,
-  todayDistance: `${BASE_URL}/distance/date/today/1d.json`,
-  todayFloors: `${BASE_URL}/floors/date/today/1d.json`,
-  todayElevation: `${BASE_URL}/elevation/date/today/1d.json`,
-  todayMinutesSedentary: `${BASE_URL}/minutesSedentary/date/today/1d.json`,
-  todayMinutesVeryActive: `${BASE_URL}/minutesVeryActive/date/today/1d.json`,
-};
-
-export const fetchActivityData = async (token) => {
+export const fetchData = async (token, endpoints) => {
   const urls = Object.values(endpoints);
   const requests = urls.map((url) =>
     axios
@@ -24,6 +12,7 @@ export const fetchActivityData = async (token) => {
       .catch((error) => ({ error }))
   );
   const responses = await Promise.all(requests);
+  //   console.log("responses", responses);
   return responses.map((response) => {
     if (response.error) {
       console.error("Error fetching activity data:", response.error);
